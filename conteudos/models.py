31155ff16aa2949a6b53fb8cec9124354d105421
Letícia Barros <rosicleidebarros07@gmail.com>
2026-08-25 09:12:36 -0300

    feat(conteudos): criar models ConteudoEducativo e CanalApoio (N:N)

--- conteudos/models.py
+++ conteudos/models.py
@@ -1,3 +1,28 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class CanalApoio(models.Model):
+    nome = models.CharField(max_length=100)       # Disque 180, Disque 100, 190...
+    descricao = models.TextField()
+    contato = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
+class ConteudoEducativo(models.Model):
+    CATEGORIA_CHOICES = [
+        ('direitos', 'Direitos'),
+        ('prevencao', 'Prevenção'),
+        ('impactos', 'Impactos psicológicos'),
+    ]
+    titulo = models.CharField(max_length=200)
+    categoria = models.CharField(max_length=20, choices=CATEGORIA_CHOICES)
+    corpo = models.TextField()
+    canais_apoio = models.ManyToManyField(CanalApoio, related_name='conteudos', blank=True)
+    publicado_em = models.DateTimeField(auto_now_add=True)
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.titulo
