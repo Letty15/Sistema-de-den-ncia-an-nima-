cc5e244ca4e54ef67205e9c9a2e1b90e376ff6b6
Eloany <eloanyrodrigues54@gmail.com>
2026-08-31 06:39:39 -0300

    criei app respostas com model RespostaDenuncia

---
+++ respostas/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin 
+from .models import RespostaDenuncia @admin.register(RespostaDenuncia) 
+
+class RespostaDenunciaAdmin(admin.ModelAdmin): list_display = ('denuncia', 'autor', 'data')
---
+++ respostas/models.py
@@ -0,0 +1,8 @@
+from django.db import models 
+from denuncias.models import Denuncia 
+from usuarios.models import Usuario 
+
+class RespostaDenuncia(models.Model): 
+    denuncia = models.ForeignKey(Denuncia, on_delete=models.CASCADE, related_name='respostas') 
+    autor = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True, related_name='respostas_dadas') 
+    mensagem = models.TextField() data = models.DateTimeField(auto_now_add=True) def __str__(self): return f'Resposta à denúncia {self.denuncia.protocolo}'
