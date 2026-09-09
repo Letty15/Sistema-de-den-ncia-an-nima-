fc689f457662b27583c29a27944c0e0f431773f4
lararg0910 <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>
2026-09-08 23:03:43 -0300

    Correção da herança

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -3,7 +3,7 @@ from .models import Usuario
 
 @admin.register(Usuario)
 class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ('user', 'cpf', 'rg', 'endereco')
-    search_fields = ('user__username', 'cpf', 'rg')
+    list_display = ('username', 'cpf', 'rg', 'endereco')
+    search_fields = ('username', 'cpf', 'rg')
     list_filter = ('endereco',)
 # Register your models here.
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,14 +1,10 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
-class Usuario(models.User):
-    user= models.OneToOneField(User, on_delete=models.CASCADE, related_name='usuario')
+class Usuario(User):
     cpf = models.CharField(max_length=14, unique=True)
-    rg = models.CharField(max_length=20, unique=True) 
+    rg = models.CharField(max_length=20, unique=True)
     endereco = models.CharField(max_length=255)
 
-
     def __str__(self):
-        return self.user.username
-
-# Create your models here.
+        return self.username
