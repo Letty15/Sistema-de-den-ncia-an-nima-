79e9bb0299d3aef30cb371d0c6c2b852ef00bbf5
lararg0910 <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>
2026-09-14 13:40:40 -0300

    Detalhar, Editar e Deletar de respostas feito

--- respostas/urls.py
+++ respostas/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('listar_respostas/<str:protocolo>/', views.listar_respostas, name='listar_respostas'),
     path('nova_resposta/<str:protocolo>/', views.nova_resposta, name='nova_resposta'),
+    path('detalhar_resposta/<int:id>/', views.detalhar_resposta, name='detalhar_resposta'),
+    path('deletar_resposta/<int:id>/', views.deletar_resposta, name='deletar_resposta'),
+    path('editar_resposta/<int:id>/', views.editar_resposta, name='editar_resposta'),
 ]
--- respostas/views.py
+++ respostas/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import get_object_or_404, render, redirect, HttpResponseRedirect
 from denuncias.models import Denuncia
+from .models import RespostaDenuncia
 from .forms import RespostaForm
 
 # Listar respostas
@@ -24,6 +25,43 @@ def nova_resposta(request, protocolo):
     else:
         form = RespostaForm()
     return render(request, 'respostas/nova_resposta.html', {'form': form, 'denuncia': denuncia})
+
+
+# Detalhar resposta
+def detalhar_resposta(request, id):
+    resposta = get_object_or_404(RespostaDenuncia, id=id)
+    return render(request, "respostas/detalhar_resposta.html", {"resposta": resposta, "denuncia": resposta.denuncia})
+
+
+# Deletar resposta
+def deletar_resposta(request, id):
+    resposta = RespostaDenuncia.objects.get(pk = id)
+    protocolo = resposta.denuncia.protocolo 
+    resposta.delete()
+    return redirect("listar_respostas", protocolo=protocolo)
+
+
+# Editar resposta
+def editar_resposta(request, id):
+    resposta = get_object_or_404(RespostaDenuncia, pk=id)
+    if request.method == "POST":
+        form = RespostaForm(request.POST, instance=resposta)
+        if form.is_valid():
+            resposta = form.save(commit=False)
+            resposta.save()
+            # força string e garante que não seja vazio
+            protocolo = str(resposta.denuncia.protocolo).strip()
+            if not protocolo:
+                protocolo = str(resposta.denuncia.id)  # fallback
+            return redirect('listar_respostas', protocolo=protocolo)
+    else:
+        form = RespostaForm(instance=resposta)
+    return render(request, "respostas/nova_resposta.html", {
+    "form": form,
+    "resposta": resposta,
+    "denuncia": resposta.denuncia
+})
+
 
 
 
