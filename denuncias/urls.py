4b0be939f7b9e365dd14a2ebf0bb5e93d371ec72
Letícia Barros <rosicleidebarros07@gmail.com>
2026-09-15 09:33:02 -0300

    Alterações Denuncia

--- denuncias/urls.py
+++ denuncias/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('listar_denuncia/', views.listar_denuncia, name='listar_denuncia'),
     path('nova_denuncia/', views.nova_denuncia, name='nova_denuncia'),
+    path('detalhar_denuncia/<str:protocolo>/', views.detalhar_denuncia, name='detalhar_denuncia'),
+    path('editar_denuncia/<str:protocolo>/', views.editar_denuncia, name='editar_denuncia'),
+    path('deletar_denuncia/<str:protocolo>/', views.deletar_denuncia, name='deletar_denuncia'),
 ]
