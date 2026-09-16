be408ba5b56233ed4656f8f97069307b0c2dceac
Leticia <rosicleidebarros07@gmail.com>
2026-09-15 22:16:46 -0300

    Atualização - funcionalidades de denúncias

--- denuncias/views.py
+++ denuncias/views.py
@@ -3,28 +3,6 @@ from .models import Denuncia
 from .forms import DenunciaForm
 import uuid
 
-def detalhar_denuncia(request, protocolo):
-    denuncia = get_object_or_404(Denuncia, protocolo=protocolo)
-    return render(request, 'denuncias/detalhar_denuncia.html', {'denuncia': denuncia})
-
-
-def editar_denuncia(request, protocolo):
-    denuncia = get_object_or_404(Denuncia, protocolo=protocolo)
-    if request.method == 'POST':
-        form = DenunciaForm(request.POST, instance=denuncia)
-        if form.is_valid():
-            form.save()
-            return redirect('detalhar_denuncia', protocolo=denuncia.protocolo)
-    else:
-        form = DenunciaForm(instance=denuncia)
-    return render(request, 'denuncias/form_denuncia.html', {'form': form, 'denuncia': denuncia})
-
-def deletar_denuncia(request, protocolo):
-    denuncia = get_object_or_404(Denuncia, protocolo=protocolo)
-    if request.method == 'POST':
-        denuncia.delete()
-        return redirect('listar_denuncia')
-    return render(request, 'denuncias/deletar_denuncia.html', {'denuncia': denuncia})
 
 def listar_denuncia(request):
     denuncias = Denuncia.objects.all()
@@ -42,3 +20,26 @@ def nova_denuncia(request):
     else:
         form = DenunciaForm()
     return render(request, 'denuncias/form_denuncia.html', {'form': form})
+
+
+def detalhar_denuncia(request, protocolo):
+    denuncia = get_object_or_404(Denuncia, protocolo=protocolo)
+    return render(request, 'denuncias/detalhar_denuncia.html', {'denuncia': denuncia})
+
+
+def editar_denuncia(request, protocolo):
+    denuncia = get_object_or_404(Denuncia, protocolo=protocolo)
+    if request.method == 'POST':
+        form = DenunciaForm(request.POST, instance=denuncia)
+        if form.is_valid():
+            form.save()
+            return redirect('detalhar_denuncia', protocolo=denuncia.protocolo)
+    else:
+        form = DenunciaForm(instance=denuncia)
+    return render(request, 'denuncias/form_denuncia.html', {'form': form, 'denuncia': denuncia})
+
+
+def deletar_denuncia(request, protocolo):
+    denuncia = get_object_or_404(Denuncia, protocolo=protocolo)
+    denuncia.delete()
+    return redirect('listar_denuncia')
